6c4e84941c5a149f0ba227305fb7119679adfd05
gabor <gabriel.molnar@student.ukf.sk>
2022-11-07 21:45:47 +0100

    feat: background added, navbar in progress

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,11 +1,4 @@
 @extends('layouts.main')
 @section('content')
     <h1>dashboard</h1>
-
-    <form id="memberform" action="">
-        <select name="member" id="members">
-
-        </select>
-    </form>
-
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE>
 <html>
     <head>
-        <title>My App</title>
+        <title>JobMeter</title>
         <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/style.css" />
     </head>
-    <body style="min-height: 100vh">
+    <body class="bg" style="min-height: 100vh">
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark container mb-4">
         <div class="container-fluid">
@@ -12,13 +13,44 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="#">Jobmeter</a>
+                <a class="navbar-brand navbarpadding_left" href="/">JobMeter</a>
+
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
                     @auth
                     <li class="nav-item">
                         <a class="nav-link" href="/praxReg">Prax</a>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
                     @endauth
+
+                    <?php if (auth()->user()?->inRole('student')): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/praxReg">Prax</a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Prax
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                        <a class="nav-link" href="/rbac/users">Prax</a>
+                    </li>
+                    <?php endif; ?>
+
                     <?php if (auth()->user()?->inRole('admin')): ?>
                     <li class="nav-item">
                         <a class="nav-link" href="/rbac/users">RBAC</a>
@@ -26,6 +58,7 @@
                     <?php endif; ?>
 
                 </ul>
+
                 <ul class="navbar-nav ">
                     <?php if (auth()->user()): ?>
                     <li class="nav-item">
@@ -44,7 +77,7 @@
         </div>
     </nav>
 
-    <div class="container">
+    <div class="container blue">
         @yield('content')
     </div>
 
