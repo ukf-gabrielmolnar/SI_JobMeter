2d671b9b77aa92dccd1997effbeb857720bdaa1b
ukf-aronmozes <aron.mozes@student.ukf.sk>
2022-11-15 16:08:44 +0100

    fix: database

--- database/migrations/2022_11_07_132449_create_contacts_table.php
+++ database/migrations/2022_11_07_132449_create_contacts_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
                 ->on('companies')
                 ->onDelete('set null');
 
+            $table->boolean('approved')->default(false);
             $table->timestamps();
         });
     }
