fb7eecd5d37d38da3e3362fd211aafd969cdc925
Kornel Takacs <kornel.takacs@student.ukf.sk>
2022-12-07 22:20:21 +0100

    fix: add filter to praxReg

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,9 +7,11 @@ use App\Models\Contract;
 use App\Models\FeedbackReport;
 use App\Models\Job;
 use App\Models\Record;
+use App\Models\Study_program;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use App\Models\Company;
+use App\Models\Year;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -27,9 +29,41 @@ class DashboardController extends Controller
     }
 
     public function praxRegistration(){
-        $companies = Company::all();
-        $jobs = Job::all();
-        $contacts = Contact::all();
+        $companyArray = [];
+        $jobArray = [];
+        $contactArray = [];
+
+        foreach (Year::all() as $year){
+            if(auth()->user()->years_id == $year->id){
+
+                foreach (Study_program::all() as $sp){
+                    if($year->study_programs_id == $sp->id){
+
+                        foreach (Job::all() as $job){
+                            if($job->study_programs_id == $sp->id && $job->approved == 1){
+                                array_push($jobArray, $job);
+
+                                foreach (Company::all() as $company){
+                                    if($job->companies_id == $company->id && $company->approved == 1){
+                                        array_push($companyArray,$company);
+
+                                        foreach (Contact::all() as $contact){
+                                            if($contact->companies_id == $company->id){
+                                                array_push($contactArray, $contact);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        $companies = array_unique($companyArray);
+        $jobs = array_unique($jobArray);
+        $contacts = array_unique($contactArray);
 
         return view('prax.praxRegistration', compact('companies','jobs','contacts'));
     }
